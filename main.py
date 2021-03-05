b5f47a6512855d42d011c42566be3c145e8dcf53
repl.it user <replituser@example.com>
2021-03-05 10:45:19 +0000

    Updated code to display a message with escape character

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-#While loop (and also FOR loop) can be used to have a repetition of a procedure in our code
+#print ("\n Displays a new line")
+#print("\t Displays a tab space")
+#print("\\ Displays a backslash")
+#print("\" Displays double quote")
+#print("\' Dispalys single quote")
 
-print("How many times to print the symbol?")
-x = int(input()) #x=3
-
-#i is a counter - it keeps track of how many times we went through the loop
-i = 0
-
-while i < x: # condition for repeating the code - as long as i lower than x
-  print("\u27BD", i)
-  i = i + 1 #new value of the counter is one more than it used to be
-
-print("We left the loop")
+print("\nI am  beep!") 
