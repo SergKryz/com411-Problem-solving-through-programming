f2345b877bc485af3a23d5d3b4b9ba57f6b092ca
repl.it user <replituser@example.com>
2021-03-11 11:41:18 +0000

    Added comparison_operators.py

--- basics/decisions/simple_decisions/modulo_operator.py
+++ basics/decisions/simple_decisions/modulo_operator.py
@@ -3,7 +3,7 @@ print("Enter the whole number.")
 
 number=int(input())
 
-if (number%2==1) :
+if (number%2==1) : #using modulo operator to find a number type
   print("The number {} is an odd number." .format(number))
 else:
   print("The number {} is an even number." .format(number))
