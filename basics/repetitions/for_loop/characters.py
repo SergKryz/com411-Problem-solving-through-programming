02e9893d1b56f7e77fd4f650507ca8034b84aad4
repl.it user <replituser@example.com>
2021-03-15 18:17:16 +0000

    Added code to demonstarte the use of FOR loop in indexing

--- basics/repetitions/for_loop/characters.py
+++ basics/repetitions/for_loop/characters.py
@@ -1,7 +1,7 @@
 print( "What strange character do you see?") #asking user 
-characters=input()
+characters=input() #user input
 print("\nIdentyfing...\n")
 
-for symbol in range(0,len(characters),1): 
+for symbol in range(0,len(characters),1): #string indexing parameters
+  
   print("index ",symbol ,":", characters[symbol] )
-
